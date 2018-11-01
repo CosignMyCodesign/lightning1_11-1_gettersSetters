dfb6f569f611793845cca92ac7705899f0e32dc5
CosignMyCodesign <dillonwilliamsail@gmail.com>
2018-11-01 09:48:14 -0500

    finished lightning exercise 1

---
+++ main.js
@@ -0,0 +1,35 @@
+// Write a Person class with a constructor that takes a first and last name.
+
+// Write a getter that returns the full name
+
+// Write a setter that sets a nickname.
+
+class Person {
+  
+  constructor (props) {
+    this.firstName = props.firstName
+    this.lastName = props.lastName
+  }
+
+  get fullName () {
+    return `${this.firstName} ${this.lastName}`
+  }
+
+  set nickName (name) {
+    this.nickName = name
+  }
+
+}
+
+const personProps = {
+  firstName: "Dillon",
+  lastName: "Williams",
+}
+
+const dillon = new Person (personProps)
+
+console.log(dillon.fullName)
+
+personProps.nickName = 'Dill Pickle'
+
+console.log(personProps.nickName)
